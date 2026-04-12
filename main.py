ff61a80c4cd733d0b2c94e29a9de08c0252de7e5
Ahmed Mohamed Shaker <140192805+A7madShaker@users.noreply.github.com>
2026-04-12 06:16:50 +0200

    Add main block to run FastAPI app
    Added main execution block to run the FastAPI app with Uvicorn.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import io
+import os
 import pickle
 import torch
 import torch.storage
@@ -76,3 +77,9 @@ async def predict(file: UploadFile = File(...)):
 
     except Exception as e:
         return JSONResponse(status_code=500, content={"error": str(e)})
+
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
